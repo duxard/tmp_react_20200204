3b36231195f8a2aeb3b5f450542f531dd3f22d7a
duxard <duxard@gmail.com>
2020-03-20 21:10:05 +0300

    --no-edit

--- src/components/HerokuList.js
+++ src/components/HerokuList.js
@@ -1,21 +1,62 @@
-import React from 'react';
-// import HerokuService from "../services/HerokuService";
+import React from "react";
+import HerokuService from "../services/HerokuService";
+
+let store = [];
 
 export default class HerokuList extends React.Component {
   constructor(props) {
     super(props);
-    // this._herokuService = new HerokuService();
+    this._herokuService = new HerokuService();
+    this.addTodo = this.addTodo.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.state = {
+      todos: [],
+      inputText: ""
+    };
+  }
+
+  addTodo(e) {
+    e.preventDefault();
+    //TODO
+  }
+
+  handleInput(e) {
+    this.setState({
+      inputText: e.target.value
+    })
   }
 
   componentDidMount() {
-    // console.log( this._herokuService.getTodos() );
+    this._herokuService.getTodos()
+      .then(data => {
+        store.push(...data);
+        this.setState({
+          todos: store
+        }, () => console.log(this.state.todos));
+      })
+      .catch(error => console.log(`Error has been caught in HerokuList: ${error}`));
   }
 
   render() {
     return (
-      <div>
+      <div className="main">
         <p>{this.props.env}</p>
-        <p>List...</p>
+        <div className="main-form">
+          <form onSubmit={this.addTodo}>
+            <input className="main-form-text"
+                   type="text"
+                   value={this.state.inputText}
+                   onChange={this.handleInput}
+            />
+            <input className="main-form-btnSubmit" type="submit"/>
+          </form>
+        </div>
+        <hr />
+        <div className="main-list">
+          {
+            this.state.todos.map(todo => <div key={todo._id}>{todo.item}</div>)
+          }
+        </div>
       </div>
     );
   }
--- src/services/HerokuService.js
+++ src/services/HerokuService.js
@@ -1,17 +1,18 @@
-// import axios from 'axios';
-//
-// export default class HerokuService {
-//   constructor() {
-//     this._apiBase = 'https://jsonplaceholder.typicode.com/users';
-//   }
-//
-//   getTodos = async (url) => {
-//     try {
-//       console.log('starting');
-//       const response = await axios.get(this._apiBase);
-//       return response;
-//   } catch(error) {
-//       console.log(error);
-//     }
-//   }
-// }
+import axios from "axios";
+
+export default class HerokuService {
+  constructor() {
+    this._apiBase = "https://asta-web-1.herokuapp.com/api/todo";
+  }
+
+  getTodos = async (url) => {
+    try {
+      const response = await axios.get(this._apiBase);
+      return response.data;
+  } catch(error) {
+      console.log(error);
+    }
+  }
+
+  postTodos() {}
+}
