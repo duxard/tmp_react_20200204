d1714a20a578b3f5d8e070f12b123107f27bb709
duxard <duxard@gmail.com>
2020-04-08 21:07:13 +0300

    add navbar menu template

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,19 @@
 import React from "react";
+import "./navbar.scss";
 
 const Navbar = () => {
  return (
-   <div>Nav template</div>
+   <div className="navbar">
+    <nav>
+     <ul className="navbar-main">
+      <li><a href="#">Link A</a></li>
+      <li><a href="#">Link B</a></li>
+      <li><a href="#">Link C</a></li>
+      <li><a href="#">Link D</a></li>
+      <li><a href="#">Link E</a></li>
+     </ul>
+    </nav>
+   </div>
  );
 };
 
