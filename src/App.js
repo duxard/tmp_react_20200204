cda9134f169d3328c5b09d2be535df9a94a0fb0f
duxard <duxard@gmail.com>
2020-04-09 19:39:48 +0300

    working on routing phase 1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import HerokuList from "./components/HerokuList/HerokuList";
 import "./app.css";
 import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./components/HomePage/HomePage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app-main">
-      <Navbar/>
-      <HerokuList env="QA"/>
-    </div>
+    <Router>
+      <div className="app-main">
+        <Navbar/>
+        <HerokuList env="QA"/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
