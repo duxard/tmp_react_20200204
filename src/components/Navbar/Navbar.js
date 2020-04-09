e2e4787d895b78da74e3a04b50df3e177e463038
duxard <duxard@gmail.com>
2020-04-09 15:34:15 +0300

    css navbar phase 2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,18 +4,16 @@ import "./navbar.scss";
 const Navbar = () => {
  return (
    <div className="navbar">
-    <div className="navbar-icon">
-      <input type="checkbox" id="menu"/>
-      <label htmlFor="menu">Menu</label>
-    </div>
     <nav>
-     <ul className="navbar-main">
-      <li><a href="#">Link A</a></li>
-      <li><a href="#">Link B</a></li>
-      <li><a href="#">Link C</a></li>
-      <li><a href="#">Link D</a></li>
-      <li><a href="#">Link E</a></li>
-     </ul>
+      <input type="checkbox" id="menu" className="navbar-checkbox" />
+      <label htmlFor="menu" className="navbar-label">Menu</label>
+      <ul className="navbar-main">
+        <li><a href="#">Link A</a></li>
+        <li><a href="#">Link B</a></li>
+        <li><a href="#">Link C</a></li>
+        <li><a href="#">Link D</a></li>
+        <li><a href="#">Link E</a></li>
+      </ul>
     </nav>
    </div>
  );
